9f70f44002b0da66ca44a08660ba02a228248ca4
Darya <darya.martyniuk@student.pk.edu.pl>
2024-05-29 21:31:06 +0200

    add all

---
+++ frontend/src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Router, Routes, Route } from "react-router";
+
+function AppRouter() {
+    <Router>
+        <Routes>
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+        </Routes>
+    </Router>
+}
+
+export default AppRouter;
